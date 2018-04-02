67e966f02f34b5a8d31dd0aac73595f3b6dc1e63
ZQian <604922962@qq.com>
2018-04-03 00:25:20 +0800

    fix

--- apps/jumpserver/settings.py
+++ apps/jumpserver/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'USER': CONFIG.DB_USER,
         'PASSWORD': CONFIG.DB_PASSWORD,
         'ATOMIC_REQUESTS': True,
-        'CONN_MAX_AGE': 900,
+        'CONN_MAX_AGE': 3600,
     }
 }
 
